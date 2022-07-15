b6cb6205db61986ffd83651ca21dc82d37a61bd8
nata <padalinka@gmail.com>
2022-07-15 11:20:34 +0200

    g

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,14 +6,30 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "natac";
-        } else { // TODO extend the programm here
+            return "natac";
+        }
+        else if (query.contains("11")){
+            return "23";
+        }
+//        else if (){
+//            return "";
+//        }
+//        else if (){
+//            return "";
+//        }
+//        else if (){
+//            return "";
+//        }
+        //        else if (){
+//            return "";
+//        }
+        else {
             return "";
         }
     }
